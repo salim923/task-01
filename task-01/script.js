bb86e53473ba3c0916646dbfd65ada3901deab31
salim923 <146474950+salim923@users.noreply.github.com>
2024-10-29 22:45:50 +0100

    Add files via upload

---
+++ task-01/script.js
@@ -0,0 +1,5 @@
+// Change navbar style on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('scrolled', window.scrollY > 50);
+});
