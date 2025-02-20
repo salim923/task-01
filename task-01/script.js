792da657e471260e1f0479ba703cd6c5f49d0209
salim923 <146474950+salim923@users.noreply.github.com>
2025-02-20 19:45:47 +0100

    Delete task-01 directory

--- task-01/script.js
+++
@@ -1,5 +0,0 @@
-// Change navbar style on scroll
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('scrolled', window.scrollY > 50);
-});
